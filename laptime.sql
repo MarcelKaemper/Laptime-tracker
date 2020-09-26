4f438bc4390f202e9bae16ae0b5e9a63dc2f8d95
Marcel Kämper <marcelkaemper862@gmail.com>
2020-09-27 00:14:21 +0200

    Added SQL Dump

---
+++ laptime.sql
@@ -0,0 +1,147 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Erstellungszeit: 27. Sep 2020 um 00:13
+-- Server-Version: 10.4.11-MariaDB
+-- PHP-Version: 7.4.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Datenbank: `laptime`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `car`
+--
+
+CREATE TABLE `car` (
+  `id` int(11) NOT NULL,
+  `brand` varchar(50) NOT NULL,
+  `model` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `game`
+--
+
+CREATE TABLE `game` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `laptime`
+--
+
+CREATE TABLE `laptime` (
+  `id` int(11) NOT NULL,
+  `time` varchar(50) NOT NULL,
+  `track_id` int(11) NOT NULL,
+  `car_id` int(11) NOT NULL,
+  `game_id` int(11) NOT NULL,
+  `created_on` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `track`
+--
+
+CREATE TABLE `track` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indizes der exportierten Tabellen
+--
+
+--
+-- Indizes für die Tabelle `car`
+--
+ALTER TABLE `car`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indizes für die Tabelle `game`
+--
+ALTER TABLE `game`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indizes für die Tabelle `laptime`
+--
+ALTER TABLE `laptime`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_laptime_track` (`track_id`),
+  ADD KEY `FK_laptime_car` (`car_id`),
+  ADD KEY `FK_laptime_game` (`game_id`);
+
+--
+-- Indizes für die Tabelle `track`
+--
+ALTER TABLE `track`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT für exportierte Tabellen
+--
+
+--
+-- AUTO_INCREMENT für Tabelle `car`
+--
+ALTER TABLE `car`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `game`
+--
+ALTER TABLE `game`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `laptime`
+--
+ALTER TABLE `laptime`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `track`
+--
+ALTER TABLE `track`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints der exportierten Tabellen
+--
+
+--
+-- Constraints der Tabelle `laptime`
+--
+ALTER TABLE `laptime`
+  ADD CONSTRAINT `FK_laptime_car` FOREIGN KEY (`car_id`) REFERENCES `car` (`id`),
+  ADD CONSTRAINT `FK_laptime_game` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`),
+  ADD CONSTRAINT `FK_laptime_track` FOREIGN KEY (`track_id`) REFERENCES `track` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
